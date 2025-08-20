0dc5d5faf88624baba52e69284b59472a72f2815
agamairi <agam.airi@gmail.com>
2025-08-20 12:50:06 -0400

    Initial Commit

---
+++ WeatherNow/WeatherNowApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeatherNowApp.swift
+//  WeatherNow
+//
+//  Created by Agam Airi on 2025-08-20.
+//
+
+import SwiftUI
+
+@main
+struct WeatherNowApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ WeatherNowTests/WeatherNowTests.swift
@@ -0,0 +1,16 @@
+//
+//  WeatherNowTests.swift
+//  WeatherNowTests
+//
+//  Created by Agam Airi on 2025-08-20.
+//
+
+import Testing
+
+struct WeatherNowTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
